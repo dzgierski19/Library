e7740cc3b1bfe5142b7f45894bc7fb5712de68c0
dzgierski19 <dzgierskigit@gmail.com>
2023-10-13 12:33:47 +0200

    modified User class

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,14 +1,15 @@
 export type userId = string;
 import { randomUUID } from "crypto";
-import { BookList } from "./BookList";
+import { clock, Clock } from "./Clock";
 
 export class User {
   id: userId = randomUUID();
-  borrowedBooks: BookList;
+  createdAt: Date;
   penaltyPoints?: number;
-  createdAt: Date = new Date();
+  banDays?: number;
+  constructor(clock: Clock) {
+    this.createdAt = clock();
+  }
 }
 
-export const Megan = new User();
-
-console.log(Megan);
+export const Megan = new User(clock);
