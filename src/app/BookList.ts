54e45fec5294b0275351ba01be902734e217fd60
dzgierski19 <dzgierskigit@gmail.com>
2023-10-13 12:36:52 +0200

    BookList

--- src/app/BookList.ts
+++ src/app/BookList.ts
@@ -1,13 +1,9 @@
 import { Book, ElonMusk, ISBN } from "./Book";
 import { ItemList } from "./ItemList";
+import { LibraryBook } from "./LibraryBook";
 
-export class BookList extends ItemList<ISBN, Book> {
+export class BookList extends ItemList<ISBN, LibraryBook> {
   constructor() {
     super("Book");
   }
 }
-
-const newBookList = new BookList();
-newBookList.addOne(ElonMusk.ISBN, ElonMusk);
-
-console.log(newBookList);
