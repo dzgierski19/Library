0a1c1da79d6a385bf58a584181c25be69a0b208a
dzgierski19 <dzgierskigit@gmail.com>
2023-10-13 12:36:24 +0200

    Booking class

--- src/app/Booking.ts
+++ src/app/Booking.ts
@@ -1,6 +1,30 @@
-class Booking {
-  constructor(
-    private readonly UserList: UserList,
-    private readonly BookList: BookList
-  ) {}
+import { ISBN } from "./Book";
+import { BookList } from "./BookList";
+import { userId } from "./User";
+import { randomUUID } from "crypto";
+
+const ALLOWED_DAYS_TO_KEEP_THE_BOOK = 10;
+
+export type BookingId = string;
+
+export class Booking {
+  id: BookingId = randomUUID();
+  booksToBorrow: BookList = new BookList();
+  booksToReturn: BookList = new BookList();
+  userId: userId;
+  constructor(private readonly bookList: BookList, userId: userId) {
+    this.userId = userId;
+  }
+
+  borrowBook(ISBN: ISBN): void {
+    const book = this.bookList.getOne(ISBN);
+    this.booksToBorrow.addOne(ISBN, book);
+  }
+
+  returnBook(ISBN: ISBN): void {
+    const book = this.bookList.getOne(ISBN);
+    this.booksToReturn.addOne(ISBN, book);
+  }
 }
+
+//date-fns
