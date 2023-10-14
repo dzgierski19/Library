c18650624a7876f2c5c7feb346533379c401683a
dzgierski19 <dzgierskigit@gmail.com>
2023-10-14 17:00:05 +0200

    deleted unnecessary variable

--- src/app/UserList.ts
+++ src/app/UserList.ts
@@ -1,10 +1,8 @@
 import { ItemList } from "./ItemList";
-import { Megan, User, userId } from "./User";
+import { User, userId } from "./User";
 
 export class UserList extends ItemList<userId, User> {
   constructor() {
     super("User");
   }
 }
-
-const newUserList = new UserList();
